5269a319fa44b6c3dfac1ce50f00ba68075e33d5
josfam <jamukun@proton.me>
2024-09-13 22:07:12 +0300

    Add models, and db connection

---
+++ back-end/models/watchlist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Watchlist model for the application"""
+
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base
+
+
+class Watchlist(Base):
+    """Models a Watchlist of a user"""
+
+    __tablename__ = 'watchlists'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    user_id = Column(ForeignKey('users.id'), Integer, nullable=False)
+    film_id = Column(Integer, nullable=False)
