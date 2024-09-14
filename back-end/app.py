718452d131fb51f23e8b18911fb58eaf000929d7
Ebube-Ochemba <ebube17@gmail.com>
2024-09-14 03:04:51 +0100

    added index and login routes

--- back-end/app.py
+++ back-end/app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+#!/usr/bin/env python3
+"""Entry point of app"""
+
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route('/')
-def home():
-    pass
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """GET route to return homepage."""
+    return jsonify({"message": "Bienvenue!"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """POST /users route for registering a new user."""
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(username, password)
+        return jsonify({"username": user.username, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "username already registered"}), 400
 
 
 if __name__ == '__main__':
