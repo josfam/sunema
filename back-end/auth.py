f42485651ccb00f7ffbf46dd8c2614caed723a86
Ebube-Ochemba <ebube17@gmail.com>
2024-09-14 03:18:43 +0100

    - added method that allows to search for user by session_id - added method that allows do delete a users session_id

--- back-end/auth.py
+++ back-end/auth.py
@@ -59,3 +59,21 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Retrieves a user based on the session ID."""
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a session by setting the session ID to None."""
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            pass
