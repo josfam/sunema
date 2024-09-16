0afd91e9da31cc745d42f6136708d3f42eabad1d
josfam <jamukun@proton.me>
2024-09-16 21:33:40 +0300

    Add films and posters to the homepage
    - Film, their titles, and their posters now render on the homepage

--- back_end/views/__init__.py
+++ back_end/views/__init__.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/')
-weather_views = Blueprint('weather_views', __name__, url_prefix='/api/v1/weather')
+weather_views = Blueprint(
+    'weather_views', __name__, url_prefix='/api/v1/weather'
+)
+film_views = Blueprint('film_views', __name__, url_prefix='/api/v1/films')
 
 from back_end.views.auth_routes import *
 from back_end.views.location_routes import *
 from back_end.views.weather import *
+from back_end.views.film_routes import *
