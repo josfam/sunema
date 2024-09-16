d78ded6c0186e6b9fffeba9e6a7bd3728610158d
ideftbuild <a.akanSWE@gmail.com>
2024-09-16 13:21:56 +0100

    feat: Implement `/api/v1/location` route and its services
    - Added `LocationService` class to handle location-related operations.
    - Implemented `get_public_ip` method to fetch the public IP address.
    - Implemented `get_location_info` method to retrieve location information based on IP address.
    - Created `/location` route in `app_views` to provide location information to clients.

--- back_end/views/__init__.py
+++ back_end/views/__init__.py
@@ -3,3 +3,4 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/')
 
 from back_end.views.auth_routes import *
+from back_end.views.location_routes import *
