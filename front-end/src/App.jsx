bae8094b3035bfb055ec0af7023d5455d61d7457
akanSWE <a.akanSWE@gamil.com>
2024-09-16 02:14:31 +0100

    feat: Implement sign up page
    - Consolidate styles from Login.css into auth.css for shared styling between Login and SignUp pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Login from "./pages/Login.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Login from './pages/Login.jsx';
+import SignUp from './pages/SignUp.jsx';
 
-function App() {
+function App () {
 
   return (
     <>
         <Router>
            <Routes>
               <Route path={'/login'} element={<Login />}></Route>
+               <Route path={'/signup'} element={<SignUp />}></Route>
            </Routes>
         </Router>
     </>
--- front-end/src/pages/SignUp.jsx
+++ front-end/src/pages/SignUp.jsx
@@ -2,16 +2,16 @@ import { useForm } from 'react-hook-form';
 import '../styles/auth.css'
 
 
-const Login = () => {
+const SignUp = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = (data) => {
-        // call service function to authenticate user
+        // call service function to save user details
         console.log(data);  // DEBUG
     };
     return (
         <div className={'auth-wrapper'}>
-            <h1 className={'welcome-text'}>Welcome</h1>
+            <h1 id={'welcome-text'}>Sign Up</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input
                     {...register('username', {required: 'Username is required'})}
@@ -23,10 +23,10 @@ const Login = () => {
                     placeholder={'Enter your password'}
                 />
                 {errors.password && <p>{errors.password.message}</p>}
-                <button type={'submit'}>Login</button>
+                <button type={'submit'}>Sign Up</button>
             </form>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
