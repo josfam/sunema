d5e7331861a5e17dc21a5fd27db1fb7663a062d4
akanSWE <a.akanSWE@gamil.com>
2024-09-17 08:13:44 +0100

    feat(auth): integrate login and sign-up pages with backend authentication.

--- front_end/src/pages/Login.jsx
+++ front_end/src/pages/Login.jsx
@@ -1,18 +1,35 @@
 import { useForm } from 'react-hook-form';
 import '../styles/auth.css'
+import {UserService} from "../services/user-service.js";
+import {useNavigate} from "react-router-dom";
 
+/**
+ * FIX: The Login component should allow users to try again after a failed login submission
+ */
 
 const Login = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, setError, clearErrors, reset } = useForm();
+    const navigate = useNavigate();
 
-    const onSubmit = (data) => {
-        // call service function to authenticate user
-        console.log(data);  // DEBUG
+    const onSubmit = async (data) => {
+        // authenticate user
+        try {
+            const response = await UserService.authenticateUser(data)
+            if (response.message === 'logged in') {
+                navigate('/');
+            }
+        } catch (error) {
+            setError('auth_error', {
+                type: 'manual',
+                message: 'Invalid username or password'
+            })
+        }
     };
     return (
         <div className={'auth-wrapper'}>
             <h1 className={'welcome-text'}>Welcome</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
+                {errors.auth_error && <p>{errors.auth_error.message}</p>}
                 <input
                     {...register('username', {required: 'Username is required'})}
                     placeholder={'Enter your Username'}
--- front_end/src/pages/SignUp.jsx
+++ front_end/src/pages/SignUp.jsx
@@ -1,18 +1,34 @@
 import { useForm } from 'react-hook-form';
+import { UserService } from '../services/user-service';
 import '../styles/auth.css'
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 
 const SignUp = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, setError } = useForm();
+    // const { error, setError } = useState('');
+    const navigate = useNavigate();
 
-    const onSubmit = (data) => {
-        // call service function to save user details
-        console.log(data);  // DEBUG
+    const onSubmit = async (data) => {
+        // register new user
+        try {
+            const response = await UserService.registerUser(data);
+            if (response.message === 'user created') {
+                navigate('/');
+            }
+        } catch (error) {
+            setError('auth_error', {
+                type: 'manual',
+                message: 'Username already exists'
+            })
+        }
     };
     return (
         <div className={'auth-wrapper'}>
             <h1 id={'welcome-text'}>Sign Up</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
+                {errors.auth_error && <p>{errors.auth_error.message}</p>}
                 <input
                     {...register('username', {required: 'Username is required'})}
                     placeholder={'Enter your Username'}
