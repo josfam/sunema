87ff594e6c555711072e87a43ef9f28986b1df6d
josfam <jamukun@proton.me>
2024-09-19 10:52:58 +0300

    Make nav and footer into components

---
+++ front_end/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import './Nav.css'
+
+function Nav() {
+	return (
+	  <div className="home__top">
+		<div id="hamburger-container">
+        	<FontAwesomeIcon icon={faBars} className="bars" aria-hidden="true" />
+		</div>
+      </div>
+	);
+}
+
+export default Nav;
