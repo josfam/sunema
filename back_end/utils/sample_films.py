08ffb589d195f924629a7183fc4e670768b50149
Joseph Amukun <jamukun@proton.me>
2024-09-27 17:36:42 +0300

    Show sample films for missing api keys (#9)
    * Add a sample film db, and logic to populate it
    
    * Refactor: Break up the movie api call url
    
    * Add a different list of default films

---
+++ back_end/utils/sample_films.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+"""Utility functions for working with the database of sample films"""
+
+from back_end.models.samples.sample_film import SampleFilm
+
--- back_end/views/film_routes.py
+++ back_end/views/film_routes.py
@@ -1,10 +1,15 @@
 import os
+import time
+import random
+from back_end.models.engine.database import db
 from random import randint
 from flask import request, jsonify
 from back_end.views import film_views
+from back_end.models.samples.sample_film import SampleFilm
 from dotenv import load_dotenv
 from back_end.utils.films import (
     MIN_RATING,
+    MAX_FILMS_RETURNED,
     years_in_the_past,
     genres_map,
     get_genres,
@@ -17,11 +22,11 @@ load_dotenv()
 
 @film_views.route('/by-weather', methods=['GET'])
 def get_films_by_weather():
+    SIMULATED_WAIT_TIME = 1
     DEFAULT_TEMPERATURE = 35
     EARLIEST_DATE = years_in_the_past(15)
-
     API_KEY = os.getenv('TMDB_API_KEY')
-    print(f'TMDB_API_KEY: {API_KEY}')  # DEBUG
+
     try:
         temp_whole = request.args['temperature'].split('.')[0]
         print(f'{temp_whole}')
@@ -29,14 +34,35 @@ def get_films_by_weather():
     except AttributeError:
         temperature = DEFAULT_TEMPERATURE
 
+    print(f'TMDB_API_KEY: {API_KEY}')  # DEBUG
+    if not API_KEY:
+        # return films from the sample database
+        all_films = {}
+        query = db.select(SampleFilm)
+        sample_films = db.session.execute(query).scalars().all()
+        random_samples = random.shuffle(sample_films)
+        for sample_film in sample_films[:MAX_FILMS_RETURNED]:
+            all_films.update(sample_film.to_dict())
+        time.sleep(SIMULATED_WAIT_TIME)
+        return jsonify({'data': all_films, 'source': 'samples'}), 200
+
     print(f'Using temperature value: {temperature}')  # DEBUG
     weather = get_temperature_desc(temperature)
     genre_ids = get_genres(weather)
 
     all_films = {}
 
+    # construct the url parameters
+    prefix = 'https://api.themoviedb.org/3/discover/movie?'
+    api = f'api_key={API_KEY}'
+    genre = 'with_genres={}'
+    sort = 'sort_by=popularity.desc'
+    vote = f'vote_average.gte={MIN_RATING}'
+    date = f'release_date.gte={EARLIEST_DATE}'
+    no_adult = f'include_adult=false'
+
     for genre_id in genre_ids:
-        url = f"https://api.themoviedb.org/3/discover/movie?api_key={API_KEY}&with_genres={genre_id}&sort_by=popularity.desc&vote_average.gte={MIN_RATING}&release_date.gte={EARLIEST_DATE}"
+        url = f"{prefix}&{api}&{vote}&{sort}&{vote}&{date}&{no_adult}" + genre.format(genre_id)
         movies = get_movies(url)
         page_count = int(
             movies['total_pages']
@@ -51,4 +77,4 @@ def get_films_by_weather():
                 all_films[movie_id] = movie
                 break
 
-    return jsonify({'data': all_films}), 200
+    return jsonify({'data': all_films, 'source': 'api'}), 200
